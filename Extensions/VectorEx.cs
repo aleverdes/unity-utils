7d30f2557e9ff21f70079567b1fe7df28946e07e
Alexander Travkin <aleverdes.alex@gmail.com>
2022-11-25 16:32:15 +0300

    Added new vector extensions

--- Extensions/VectorEx.cs
+++ Extensions/VectorEx.cs
@@ -17,5 +17,30 @@ namespace AffenCode
         {
             return new Vector3(vec.x, vec.y, 0);
         }
+
+        public static Vector3 WithX(this Vector3 vec, float x)
+        {
+            return new Vector3(x, vec.y, vec.z);
+        }
+
+        public static Vector3 WithY(this Vector3 vec, float y)
+        {
+            return new Vector3(vec.x, y, vec.z);
+        }
+
+        public static Vector3 WithZ(this Vector3 vec, float z)
+        {
+            return new Vector3(vec.x, vec.y, z);
+        }
+
+        public static Vector2 WithX(this Vector2 vec, float x)
+        {
+            return new Vector2(x, vec.y);
+        }
+
+        public static Vector2 WithY(this Vector2 vec, float y)
+        {
+            return new Vector2(vec.x, y);
+        }
     }
 }
