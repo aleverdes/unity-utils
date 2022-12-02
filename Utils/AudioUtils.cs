2dc038a7cdd51f460a8ffcdfbcde98fa08ede8b3
Alexander Travkin <aleverdes.alex@gmail.com>
2022-12-02 12:23:17 +0300

    Added AudioUtils

---
+++ Utils/AudioUtils.cs
@@ -0,0 +1,23 @@
+using AffenCode;
+using UnityEngine;
+
+namespace AffenCode
+{
+    public static class AudioUtils
+    {
+        public const string AudioMixerMasterVolumeParameter = "MasterVolume";
+        public const string AudioMixerSoundVolumeParameter = "SoundVolume";
+        public const string AudioMixerMusicVolumeParameter = "MusicVolume";
+        public const string AudioMixerVoiceChatVolumeParameter = "VoiceChatVolume";
+        
+        public static float ConvertValueToVolume(float value, float volumeModifier = 1f)
+        {
+            if (value <= FloatEpsilon.Value)
+            {
+                return -80f;
+            }
+
+            return Mathf.Log10(value * volumeModifier) * 20f;
+        }
+    }
+}
