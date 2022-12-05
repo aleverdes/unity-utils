3dc36cf79f6905b27d3d7f127893c901abd01f12
Alexander Travkin <aleverdes.alex@gmail.com>
2022-12-05 10:16:02 +0300

    Added Vector2.Rotate(float)

--- Extensions/VectorEx.cs
+++ Extensions/VectorEx.cs
@@ -42,5 +42,17 @@ namespace AffenCode
         {
             return new Vector2(vec.x, y);
         }
+
+        public static Vector2 Rotate(this Vector2 v, float degrees)
+        {
+            var sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+            var cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+            var tx = v.x;
+            var ty = v.y;
+            v.x = cos * tx - sin * ty;
+            v.y = sin * tx + cos * ty;
+            return v;
+        }
     }
 }
