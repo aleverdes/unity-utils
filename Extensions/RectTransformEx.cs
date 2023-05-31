73b13170f6de0c334a24256c3e6ab1331e08e0b9
Alexander Travkin <aleverdes.alex@gmail.com>
2023-06-01 00:28:45 +0400

    New method with canvas scale factor

--- Extensions/RectTransformEx.cs
+++ Extensions/RectTransformEx.cs
@@ -15,6 +15,20 @@ namespace AffenCode
             return new Rect(topLeft, scaledSize);
         }
         
+        public static Rect GetWorldRect(this RectTransform rectTransform, float scale)
+        {
+            Vector3[] corners = new Vector3[4];
+            rectTransform.GetWorldCorners(corners);
+            // Get the bottom left corner.
+            Vector3 position = corners[0];
+         
+            Vector2 size = new Vector2(
+                rectTransform.lossyScale.x * rectTransform.rect.size.x,
+                rectTransform.lossyScale.y * rectTransform.rect.size.y);
+ 
+            return new Rect(position * (1f / scale), size * (1f / scale));
+        }
+        
         public static Rect GetWorldRect(this RectTransform rectTransform)
         {
             Vector3[] corners = new Vector3[4];
