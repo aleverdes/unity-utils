d206a5478abbace93696ec5a0ee99c970f5e3c5d
Alexander Travkin <aleverdes.alex@gmail.com>
2023-12-15 17:32:50 +0300

    ToVector2 and ToVector3 for int vectors

--- Extensions/VectorEx.cs
+++ Extensions/VectorEx.cs
@@ -74,6 +74,21 @@ namespace AleVerDes.UnityUtils
         {
             return new Vector3(0, v.y, 0);
         }
+        
+        public static Vector2 ToVector2(this Vector2Int v)
+        {
+            return new Vector2(v.x, v.y);
+        }
+        
+        public static Vector2Int ToVector2Int(this Vector2 v)
+        {
+            return new Vector2Int((int)v.x, (int)v.y);
+        }
+        
+        public static Vector3 ToVector3(this Vector3Int v)
+        {
+            return new Vector3(v.x, v.y, v.z);
+        }
 
         public static float GetRandom(this Vector2 v)
         {
